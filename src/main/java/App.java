373168363dbfdec5916fbf126d98d588cabee053
Ева <eviksa13@mail.ru>
2026-04-21 19:14:46 +0300

    Выполнить создание класса Cat и проверку на равенство бинов

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean2.getMessage());
         System.out.println(bean.getMessage());
+        System.out.println("-----Обьекты одинаковы? " + (bean2 == bean) + "------");
+
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
+        System.out.println(catBean.getSay());
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(catBean2.getSay());
+        System.out.println("-------Обьекты одинаковы?  " + (catBean == catBean2) + "------");
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+
+    public String say;
+
+    public void setSay(String say) {this.say = say;}
+
+    public String getSay(){return say;}
+
+}
